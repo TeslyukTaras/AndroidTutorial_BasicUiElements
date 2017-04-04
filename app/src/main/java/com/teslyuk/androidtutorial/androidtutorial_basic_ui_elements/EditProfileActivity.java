1580310674419254cec8298818a5483862373a39
Teslyuk Taras <taras.teslyuk@gmail.com>
2017-04-04 11:56:10 +0300

    add: initial source code and project configurations;

---
+++ app/src/main/java/com/teslyuk/androidtutorial/androidtutorial_basic_ui_elements/EditProfileActivity.java
@@ -0,0 +1,17 @@
+package com.teslyuk.androidtutorial.androidtutorial_basic_ui_elements;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by taras.teslyuk on 3/30/17.
+ */
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
---
+++ app/src/main/java/com/teslyuk/androidtutorial/androidtutorial_basic_ui_elements/ProfileActivity.java
@@ -0,0 +1,25 @@
+package com.teslyuk.androidtutorial.androidtutorial_basic_ui_elements;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ImageView imageView = (ImageView) findViewById(R.id.edit_profile_iv);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
